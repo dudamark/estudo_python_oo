b09d1fd97100369b8a715792056ccda201f4553e
dudamark <dudamark75@gmail.com>
2024-07-29 11:19:34 -0300

    Implementação de herança, polimorfismo e métodos abstratos

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Estudo sobre como aplicar Orientção a Objetos no Python.
 
 Aborda Classes, métodos construtores, métodos especiais, importar classes em arquivos Python, criar atributos em classes, utilizar underscore (underline) para indicar que um atributo é protegido e  uso da função property.
 
-Em breve novas atualizações.
+Nessa segunda parte abordo os temas de herânça, polimorfismo e métodos abstratos.
+
+
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
-
-restaurante_cactus = Restaurante('cactus', 'Mexicana')
-restaurante_cactus.alternar_estado()
-restaurante_cactus.receber_avaliacao('Duda', 10)
-restaurante_cactus.receber_avaliacao('Mario', 5)
-restaurante_cactus.receber_avaliacao('Amoreco', 8)
-
+bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
